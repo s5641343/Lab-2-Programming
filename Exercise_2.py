b34a071f0f9fabd0e3cdfbbccf2efdb68e00b09d
Robbie Howatson Howatson (s5641343) <s5641343@bournemouth.ac.uk>
2024-10-09 13:39:18 +0100

    initial message

---
+++ Exercise_2.py
@@ -0,0 +1,8 @@
+name = "Robert"
+print(name)
+age = 20
+print(age)
+hobbies = "Boxing, Gaming"
+print(hobbies)
+unit_num = 6
+print(unit_num)
---
+++ python_lab2.py
@@ -0,0 +1,97 @@
+a = 11
+a +=5
+type_a = type(a)
+print(f"a = {a} and its data type is {type_a}")
+
+a = 11
+b = 5
+result = a >= b
+type_result = type(result)
+print(f"result = {result} and its data type is {type_result}")
+
+unit_name = "level 4 programming"
+print("This unit is: ", unit_name)
+print("The 1st character is:", unit_name[0])
+print("The 4th character is:", unit_name[3])
+print("The 9th character is:", unit_name[8])
+
+greeting_string = "Welcome to Programming unit! This is Level 4 unit :)"
+print(greeting_string)
+print("The 1st character is", greeting_string[0])
+print("The 5th character is", greeting_string[4])
+print(greeting_string[0:8])
+print(greeting_string[-1])
+print(greeting_string[-8:])
+
+a = 11
+print(type(a))
+
+a = 11.0
+print(type(a))
+
+a = "11" + "11"
+print(type(a))
+
+a = True
+print(type(a))
+
+x = 10.5 
+print(type(x))
+x = int(x)
+print(type(x))
+
+x = "Hey"
+y = 4
+z = 3.2
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
+result = y + z
+print(result)
+print(type(result))
+result = y + int(z)
+print(result)
+print(type(result))
+result = z + float(y)
+print(result)
+print(type(result))
+print(type(str(z)))
+result = x + str(y) + str(z)
+print(result)
+print(type(result))
+
+my_var = 1
+name = "Robert"
+print(name)
+age = 20
+print(age)
+hobbies = "Boxing, Gaming"
+print(hobbies)
+unit_num = 6
+print(unit_num)
+
+my_text = "Hello, World"
+my_text = my_text.upper()
+print(my_text)
+
+my_text = "I have an apple, you have an apple"
+x = my_text.replace("apple", "orange", 1)
+print(x)
+
+my_text = "   I have an apple, you have an apple   "
+x = my_text.strip()
+print("yes", x, "hey")
+
+my_text = "python Java C++"
+x = my_text.split()
+print(x)
+
+my_text = "Bananas are great source of potassium"
+x = my_text.count("Bananas")
+print(x)
+
+a, b = 10, 3
+print("a + b =", a + b)
+
+https://github.com/s5641343/Lab-2-Programming.git
